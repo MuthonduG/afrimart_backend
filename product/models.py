0731d5366ecc87b35f5fc051f5c4398b6782c539
MuthonduG <muthondugithinji@gmail.com>
2025-11-15 02:05:15 +0300

    updates: update user models and serializers

--- product/models.py
+++ product/models.py
@@ -1,15 +1,36 @@
 from django.db import models
-from django.core.validators import MinLengthValidator
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 from user_auth.models import User
 
 
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products')
-    product_title = models.CharField( max_length=256 ,validators=[MinLengthValidator(10)])
+    product_name = models.CharField( max_length=256 ,validators=[MinLengthValidator(10)])
     product_description = models.TextField(max_length=256, validators=[MinLengthValidator(30)])
     product_price = models.CharField(max_length=6)
+    discount_percentage = models.IntegerField( validators=[MinValueValidator(0), MaxValueValidator(100)], default=0,help_text="Discount percentage (0-100)")
+    product_brand = models.CharField(max_length=100)
+    product_category = models.CharField(max_length=100)
     product_qty = models.IntegerField(default=0)
     product_image_url = models.CharField(max_length=256, null=True, blank=True)
+    product_rating = models.DecimalField(max_digits=3, decimal_places=2, validators=[MinValueValidator(0), MaxValueValidator(5)], default=0)
+    product_features = models.ArrayField(models.CharField(max_length=200), blank=True, default=list)
+    product_specifications = models.DictField( blank=True, default=dict)
+    product_colors = models.ArrayField( models.CharField(max_length=50), blank=True, default=list )
+    product_warranty = models.DictField( default=dict, blank=True )
+    deal_tag = models.CharField(
+        max_length=50, 
+        blank=True, 
+        null=True, 
+        choices=[
+            ('Flash', 'Flash Sale'),
+            ('Clearance', 'Clearance'),
+            ('Deal', 'Special Deal'),
+            ('Bundle', 'Bundle Offer'),
+        ],
+        help_text="Type of deal if applicable"
+    )
+    is_new = models.BooleanField(default=False)
     date_created = models.DateTimeField(auto_now_add=True, editable=False)
 
     def __str__(self):
--- product/serializers.py
+++ product/serializers.py
@@ -4,8 +4,27 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'product_title', 'product_description', 'product_price', 'date_created', 'user']
-        read_only_fields = ['user'] 
+        fields = [
+            'id', 
+            'user',
+            'product_name', 
+            'product_description', 
+            'product_price', 
+            'discount_percentage',
+            'product_brand',
+            'product_category',
+            'product_qty',
+            'product_image_url',
+            'product_rating',
+            'product_features',
+            'product_specifications',
+            'product_colors',
+            'product_warranty',
+            'deal_tag',
+            'is_new',
+            'date_created'
+        ]
+        read_only_fields = ['user', 'date_created'] 
 
     def create(self, validated_data):
         validated_data['user'] = self.context['request'].user
